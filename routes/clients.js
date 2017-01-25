4c62a8ad5a38b551e2a7984d6e67245dff168359
Andy Josephson <andy.josephson@derelictmansion.com>
2017-01-25 12:36:28 -0700

    adding a bunch of Angular setup files needed later

--- routes/clients.js
+++ routes/clients.js
@@ -23,4 +23,62 @@ router.get('/client/:id', function (req, res, next) {
     });
 });
 
+//Save Tasks
+router.post('/client', function(req,res,next){
+  var client = req.body;
+  if(!client.fName){
+    res.status(400);
+    res.json({
+      "error": "Bad Data"
+    });
+  } else{
+    db.clients.save(client, function(err, client){
+      if(err){
+          res.send(err)
+      }
+      res.json(client);
+    })
+  }
+});
+
+
+//Delete client
+router.delete('/client/:id', function(req,res,next){
+  db.clients.remove({_id: mongojs.ObjectId(req.params.id)},function (err,client) {
+    if(err){
+      res.send(err);
+    }
+    res.json(client);
+  })
+});
+
+//Update client
+router.put('/client/:id', function(req,res,next){
+  var client = req.body;
+  var updTask = {};
+
+if(client.fName){
+  updTask.fName = client.fName;
+}
+
+if(client.lName){
+  updTask.lName = client.lName;
+}
+
+if(!updTask){
+  res.status(400);
+  res.json({
+    "error": "Bad Data"
+  })
+ } else {
+    db.clients.update({_id: mongojs.ObjectId(req.params.id)},updTask, {},function (err,client) {
+      if(err){
+        res.send(err);
+      }
+      res.json(client);
+    });
+}
+});
+
+
 module.exports = router
